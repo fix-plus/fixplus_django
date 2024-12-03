dde2b6390420f53d81e122a5425f7f50897d7764
Ali <a95blnd@gmail.com>
2024-12-03 23:43:25 +0330

    change cors settings

--- config/settings/cors.py
+++ config/settings/cors.py
@@ -1,2 +1,9 @@
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:64012',
+    'http://127.0.0.1:64012',
+    'https://fixplus.org',
+    'https://www.fixplus.org'
+]
