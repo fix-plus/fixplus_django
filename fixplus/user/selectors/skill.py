5c26d8cfce0d37d6173e559094454a96a7ffa08a
Ali <a95blnd@gmail.com>
2024-12-31 14:26:13 +0330

    add sort and order to user skill

--- fixplus/user/selectors/skill.py
+++ fixplus/user/selectors/skill.py
@@ -8,6 +8,8 @@ def search_technician_skill(
         technician_id:  str | None = None,
         device_type: str | None = None,
         brand_names:list | None = None,
+        sort_by: str = 'created_at',
+        order: str = 'desc'  # Default to descending order
 ):
 
     queryset = TechnicianSkill.objects.all()
@@ -22,7 +24,17 @@ def search_technician_skill(
         instance_brand_names = BrandNameParametric.objects.filter(title__in=brand_names)
         queryset = queryset.filter(brand_names__in=instance_brand_names)
 
-    return queryset.order_by('-created_at')
+    # Determine the order direction
+    order_prefix = '-' if order == 'desc' else ''
+
+    # Sort the results if sort_by is provided
+    if sort_by:
+        if sort_by in ['created_at', 'updated_at', ]:
+            queryset = queryset.order_by(f"{order_prefix}{sort_by}")
+        else:
+            raise ValueError("Invalid sort_by value. Must be one of: 'created_at', 'updated_at',.")
+
+    return queryset
 
 
 def get_technician_skill(
--- fixplus/user/serializers/skill.py
+++ fixplus/user/serializers/skill.py
@@ -8,6 +8,8 @@ from fixplus.user.models import TechnicianSkill
 class InputTechnicianSkillParamsSerializer(serializers.Serializer):
     technician_id = serializers.UUIDField(required=True)
     device_type = serializers.CharField(required=False, allow_null=True, default=None)
+    sort_by = serializers.ChoiceField(required=False, default=None, choices=['created_at', 'updated_at'])
+    order = serializers.CharField(required=False, default=None)
 
 
 class InputTechnicianSkillSerializer(serializers.Serializer):
